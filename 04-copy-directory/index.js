2f432f1f301032b9b2c920a270be6a898aca301b
maria.akulova <maria.akulova.dragon@gmail.com>
2024-01-21 21:18:53 +0100

    feat: 04-copy-directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,38 @@
+const { mkdir, rm, readdir, copyFile } = require('fs/promises');
+const { join } = require('path');
+
+const targetFolder = join(__dirname, 'files-copy');
+const currentFolder = join(__dirname, 'files');
+
+function createTargetDirectory() {
+  mkdir(targetFolder, {
+    recursive: true,
+  });
+}
+
+function copy(fileName) {
+  copyFile(join(currentFolder, fileName), join(targetFolder, fileName));
+}
+
+function copyFileToTargetFolder() {
+  readdir(currentFolder, {
+    withFileTypes: true,
+  }).then((files) => {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        copy(file.name);
+      }
+    });
+  });
+}
+
+rm(targetFolder, {
+  recursive: true,
+  force: true,
+})
+  .then(function () {
+    createTargetDirectory();
+  })
+  .finally(() => {
+    copyFileToTargetFolder();
+  });

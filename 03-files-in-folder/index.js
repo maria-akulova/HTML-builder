9e0077f743612d9a83ee71c0ee514163d43e85cb
maria.akulova <maria.akulova.dragon@gmail.com>
2024-01-21 22:45:54 +0100

    refactor: imrove logic of different file name style: 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,8 +10,18 @@ function printFileInfo(item, currentFolder) {
 
   if (item.isFile()) {
     fs.stat(fileStat, function (errStat, stats) {
-      const fileName = item.name.split('.').slice(0, -1).join('.');
-      const fileExt = extname(item.name).slice(1);
+      let fileName = item.name.split('.').slice(0, -1).join('.');
+      if (!fileName) {
+        fileName = item.name;
+      }
+      if (item.name.split('.')[0].length === 0) {
+        fileName = '';
+      }
+      let fileExt = extname(item.name).slice(1);
+      if (!fileExt && item.name.split('.')[0].length === 0) {
+        fileExt = item.name.split('.')[1];
+      }
+
       const fileSize = stats.size / 1024;
       stdout.write(`${fileName} - ${fileExt} - ${fileSize} Kb\n`);
       if (errStat)

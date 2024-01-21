bd6c636d444c7fb0c1c965f547bd19b093dc414a
maria.akulova <maria.akulova.dragon@gmail.com>
2024-01-21 22:23:10 +0100

    refactoring: small improvement of 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,20 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+const { createWriteStream } = require('fs');
+const { join } = require('path');
 const { stdin, stdout, exit } = require('process');
 
 const newFileName = 'userData.txt';
-const writeFile = fs.createWriteStream(path.join(__dirname, newFileName));
+const writeFile = createWriteStream(join(__dirname, newFileName));
 
 function stopWritingFile() {
-    stdout.write(`\nYour data was saving to the disk within file ${newFileName}`);
-    exit();
+  stdout.write(
+    `\nYour data was saving to the disk within the file ${newFileName}`,
+  );
+  exit();
 }
 
 stdout.write('\nPlease, type in the data you want to save on the disk:\n');
 stdin.on('data', (data) => {
-    if (data.toString().trim().toLowerCase() === 'exit') {
-        stopWritingFile();
-    }
-    writeFile.write(data);
+  const dataContent = data.toString().trim().toLowerCase();
+  if (dataContent === 'exit') {
+    stopWritingFile();
+  }
+  writeFile.write(data);
 });
 process.on('SIGINT', stopWritingFile);
